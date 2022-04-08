49ea747a3a32de7666e0442cb3b4f953f80e8651
Andreas Houben <andreas.houben@codecentric.de>
2022-04-08 13:24:20 +0200

    ignore coverage of mock files

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
   preset: "jest-preset-angular",
   setupFilesAfterEnv: ["<rootDir>/setup-jest.ts"],
   globalSetup: "jest-preset-angular/global-setup",
+  coveragePathIgnorePatterns: ["/node_modules/", "<rootDir>/src/mocks"],
   coverageThreshold: {
     global: {
       branches: 80,
